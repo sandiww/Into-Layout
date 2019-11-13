0103c27c79a16512f244643e4defb608775c8841
unknown <sandirpl3e@gmail.com>
2019-11-13 12:57:39 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:intro_layout/screens/intro_view.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Into Layout',
+      home: IntroPage(),
+  ));
+}
+
